1d7147b6974942f2723bc73ca5651eb121189e70
UnfazedAy <ayomidesoniyi@gmail.com>
2023-02-02 22:49:26 +0100

    Done with task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Task 5 module """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     performs a sum operation
     Args:
