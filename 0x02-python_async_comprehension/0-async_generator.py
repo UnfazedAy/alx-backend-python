8ce6883a6d68b5c2c6cf28e3ee238ab33306e3e7
UnfazedAy <ayomidesoniyi@gmail.com>
2023-02-07 16:49:06 +0100

    Completed task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,4 +10,4 @@ async def async_generator() -> Generator[float, None, None]:
     """ Genrates a random number between 0 and 10 """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
