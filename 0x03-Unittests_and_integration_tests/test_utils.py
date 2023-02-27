0fc050e1d6c9b5680196fba2f1230bbb186fcc86
UnfazedAy <ayomidesoniyi@gmail.com>
2023-02-27 20:20:45 +0100

    Done with task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,54 +1,53 @@
 #!/usr/bin/env python3
-"""Module for task 0"""
-
+"""This file contains the TestAccessNestedMap class"""
 import unittest
-from unittest.mock import Mock, patch
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
 from parameterized import parameterized, parameterized_class
-from utils import access_nested_map, get_json
-from typing import Dict, Union, Mapping, Sequence, Any, Callable
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """
-    A class that uses unittest and parmetrization to verify
-    utils.access_nested_map
-    """
-
-    # start of task 0
+    """The TestAccessNestedMap class"""
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": 1}, ('a', ), 1),
+        ({"a": {"b": 2}}, ('a', ), {"b": 2}),
+        ({"a": {"b": 2}}, ('a', 'b'), 2)
     ])
     def test_access_nested_map(
         self,
         nested_map: Mapping,
         path: Sequence,
-        expected_result: Any
+        result: Any
     ) -> Any:
         """
         A method that tests the accesss_nested_map by checking if the
         nested_map is equal to the path and expected result.
         """
-        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+        self.assertEqual(access_nested_map(nested_map, path), result)
 
-    # start of task 1
     @parameterized.expand([
-        ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError)
+        ({}, ('a'), KeyError),
+        ({"a": 1}, ('a', 'b'), KeyError),
     ])
-    def test_access_nested_map_error(
+    def test_access_nested_map_exception(
         self,
         nested_map: Mapping,
         path: Sequence,
-        expected_result: Any
+        result: Any
     ) -> Any:
         """
         A method that tests the accesss_nested_map by checking if there's
         an Exception Error and hence uses the assertRaises method
         to throw an errow message
         """
-        with self.assertRaises(expected_result):
+
+        with self.assertRaises(result):
             access_nested_map(nested_map, path)
 
 
