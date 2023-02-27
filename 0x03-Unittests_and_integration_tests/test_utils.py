7ec6afce26cbc0b240de24cdd0fdef7d13da090f
UnfazedAy <ayomidesoniyi@gmail.com>
2023-02-27 21:45:55 +0100

    Done with task 1/bin/python3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -73,7 +73,7 @@ class TestGetJson(unittest.TestCase):
         mock_response.json.return_value = test_payload
         mock_request.get_return_value = mock_response
 
-        self.assertNotEqual(get_json(test_url), test_payload)
+        self.assertTrue(get_json(test_url), test_payload)
 
 
 if __name__ == "__main__":
