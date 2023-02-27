8b6e9cc48d2f18c84478c238c5f1ccb45a8dcbae
UnfazedAy <ayomidesoniyi@gmail.com>
2023-02-27 12:01:24 +0100

    Done with task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,84 +1,55 @@
 #!/usr/bin/env python3
-"""A module for testing the utils module.
-"""
-import unittest
-from typing import Dict, Tuple, Union
-from unittest.mock import patch, Mock
-from parameterized import parameterized
+"""Module for task 0"""
 
-from utils import (
-    access_nested_map,
-    get_json,
-    memoize,
-)
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+from typing import Dict, Union, Mapping, Sequence, Type
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Tests the `access_nested_map` function."""
+    """
+    A class that uses unittest and parmetrization to verify
+    utils.access_nested_map
+    """
+
+    # start of task 0
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(
-            self,
-            nested_map: Dict,
-            path: Tuple[str],
-            expected: Union[Dict, int],
-            ) -> None:
-        """Tests `access_nested_map`'s output."""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+        self,
+        nested_map: Mapping,
+        path: Sequence,
+        expected_result: Union[Dict, int]
+    ) -> None:
+        """
+        A method that tests the accesss_nested_map by checking if the
+        nested_map is equal to the path and expected result.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
+    # start of task 1
     @parameterized.expand([
         ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
     ])
-    def test_access_nested_map_exception(
-            self,
-            nested_map: Dict,
-            path: Tuple[str],
-            exception: Exception,
-            ) -> None:
-        """Tests `access_nested_map`'s exception raising."""
-        with self.assertRaises(exception):
+    def test_access_nested_map_error(
+        self,
+        nested_map: Dict,
+        path: Sequence,
+        expected_result: Type[Exception]
+    ) -> None:
+        """
+        A method that tests the accesss_nested_map by checking if there's
+        an Exception Error and hence uses the assertRaises method
+        to throw an errow message
+        """
+        with self.assertRaises(expected_result):
             access_nested_map(nested_map, path)
 
 
-class TestGetJson(unittest.TestCase):
-    """Tests the `get_json` function."""
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False}),
-    ])
-    def test_get_json(
-            self,
-            test_url: str,
-            test_payload: Dict,
-            ) -> None:
-        """Tests `get_json`'s output."""
-        attrs = {'json.return_value': test_payload}
-        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
-            self.assertEqual(get_json(test_url), test_payload)
-            req_get.assert_called_once_with(test_url)
-
-
-class TestMemoize(unittest.TestCase):
-    """Tests the `memoize` function."""
-    def test_memoize(self) -> None:
-        """Tests `memoize`'s output."""
-        class TestClass:
-            def a_method(self):
-                return 42
-
-            @memoize
-            def a_property(self):
-                return self.a_method()
-        with patch.object(
-                TestClass,
-                "a_method",
-                return_value=lambda: 42,
-                ) as memo_fxn:
-            test_class = TestClass()
-            self.assertEqual(test_class.a_property(), 42)
-            self.assertEqual(test_class.a_property(), 42)
-            memo_fxn.assert_called_once()
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
