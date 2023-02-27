0cdc5a494f0b2a90d982f3cd556dc1b6c74f7ec6
UnfazedAy <ayomidesoniyi@gmail.com>
2023-02-27 22:25:34 +0100

    Done with task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,7 +10,7 @@ from typing import (
 )
 from unittest.mock import MagicMock, patch
 from parameterized import parameterized, parameterized_class
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 # start of task 0
@@ -74,6 +74,29 @@ class TestGetJson(unittest.TestCase):
         mock_request.get_return_value = mock_response
 
         self.assertTrue(get_json(test_url), test_payload)
+
+
+# start of task 3
+class TestMemoize(unittest.TestCase):
+    """Tests the `memoize` function."""
+    def test_memoize(self) -> None:
+        """Tests `memoize`'s output."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
 
 
 if __name__ == "__main__":
