1be2182daf83aeed2b0e6f125eb787d0532d58ea
UnfazedAy <ayomidesoniyi@gmail.com>
2023-02-27 11:28:36 +0100

    Done with task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
-from typing import Dict, Tuple, Union, Mapping, Sequence
+from typing import Dict, Union, Mapping, Sequence
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -13,6 +13,7 @@ class TestAccessNestedMap(unittest.TestCase):
     utils.access_nested_map
     """
 
+    # start of task 0
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -29,6 +30,25 @@ class TestAccessNestedMap(unittest.TestCase):
         nested_map is equal to the path and expected result.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    # start of task 1
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_error(
+        self,
+        nested_map: Mapping,
+        path: Sequence,
+        expected_result: Exception
+    ) -> None:
+        """
+        A method that tests the accesss_nested_map by checking if there's
+        an Exception Error and hence uses the assertRaises method
+        to throw an errow message
+        """
+        with self.assertRaises(expected_result):
+            access_nested_map(nested_map, path)
 
 
 if __name__ == "__main__":
